c199345f35dc44d586e9849460ed8118bc40d2ea
MattMcKay <matthewmckay@gmail.com>
2023-07-10 16:06:48 -0400

    Working out navigation set up using documentation from react router official site. Have a home page and error page set up so far

--- vite-chatvia-project/src/main.jsx
+++ vite-chatvia-project/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import ErrorPage from './pages/ErrorPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div className='home'>Hellow world</div>,
+    errorElement: <ErrorPage />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
