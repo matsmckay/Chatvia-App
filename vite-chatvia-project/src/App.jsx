b540623d466c5e98ed7e0a37465d49a0ec10083f
MattMcKay <matthewmckay@gmail.com>
2023-07-10 21:44:56 -0400

    Placeholders for app navigation in place with text titles. The components are also currently just text as well

--- vite-chatvia-project/src/App.jsx
+++ vite-chatvia-project/src/App.jsx
@@ -6,28 +6,39 @@ import Chats from './pages/Chats'
 import GroupChats from './pages/GroupChats'
 import Contacts from './pages/Contacts'
 import Settings from './pages/Settings'
+import DarkMode from './pages/DarkMode'
 
 function App() {
   return (
     <BrowserRouter>
       <header>
-        <nav>
-          <h1>ChatVia</h1>
-          <h2>
-            <Link to='/'>Profile</Link>
-          </h2>
-          <h2>
-            <NavLink to='chats'>Chats</NavLink>
-          </h2>
-          <h2>
-            <NavLink to='groupchats'>Group Chats</NavLink>
-          </h2>
-          <h2>
-            <NavLink to='contacts'>Contacts</NavLink>
-          </h2>
-          <h2>
-            <NavLink to='settings'>Settings</NavLink>
-          </h2>
+        <nav className='nav-container'>
+          <h1 className='nav-top'>ChatVia</h1>
+          <div className='navLink-container'>
+            <h2>
+              <NavLink to='/'>Profile (icon button)</NavLink>
+            </h2>
+            <h2>
+              <NavLink to='chats'>Chats</NavLink>
+            </h2>
+            <h2>
+              <NavLink to='groupchats'>Group Chats</NavLink>
+            </h2>
+            <h2>
+              <NavLink to='contacts'>Contacts</NavLink>
+            </h2>
+            <h2>
+              <NavLink to='settings'>Settings</NavLink>
+            </h2>
+          </div>
+          <div className='nav-bottom'>
+            <h2>
+              <NavLink to='darkmode'>DarkMode</NavLink>
+            </h2>
+            <h2>
+              <NavLink to='/'>Profile (photo)</NavLink>
+            </h2>
+          </div>
         </nav>
       </header>
       <main>
@@ -37,6 +48,8 @@ function App() {
           <Route path='groupchats' element={<GroupChats />} />
           <Route path='contacts' element={<Contacts />} />
           <Route path='settings' element={<Settings />} />
+          <Route path='darkmode' element={<DarkMode />} />
+          <Route path='profile' element={<Profile />} />
         </Routes>
         <p>Chat component</p>
       </main>
---
+++ vite-chatvia-project/src/pages/DarkMode.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DarkMode = () => {
+  return (
+    <div>
+      <h1>DarkMode yall</h1>
+    </div>
+  )
+}
+
+export default DarkMode
