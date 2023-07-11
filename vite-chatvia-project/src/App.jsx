1be26895f53438a2cda4993f1c44dea99302c1d6
MattMcKay <matthewmckay@gmail.com>
2023-07-11 07:06:28 -0400

    Have a basic html layout of the chat navigation, section for rendering whatever nav link is selected and a component for the current conversation

--- vite-chatvia-project/src/App.jsx
+++ vite-chatvia-project/src/App.jsx
@@ -1,5 +1,16 @@
 import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom'
 
+// icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faUser,
+  faUserGroup,
+  faAddressBook,
+  faCircleHalfStroke,
+  faGear,
+  faCommentDots,
+} from '@fortawesome/free-solid-svg-icons'
+
 // pages
 import Profile from './pages/Profile'
 import Chats from './pages/Chats'
@@ -8,64 +19,68 @@ import Contacts from './pages/Contacts'
 import Settings from './pages/Settings'
 import DarkMode from './pages/DarkMode'
 
-// icons
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-import { faUserGroup } from '@fortawesome/free-solid-svg-icons'
-import { faAddressBook } from '@fortawesome/free-solid-svg-icons'
-
 function App() {
   return (
-    <BrowserRouter>
-      <header>
-        <nav className='nav-container'>
-          <h1 className='nav-top'>ChatVia</h1>
-          <div className='navLink-container'>
-            <h2>
-              <NavLink to='/'>
-                <FontAwesomeIcon icon={faUser} />
-              </NavLink>
-            </h2>
-            <h2>
-              <NavLink to='chats'>Chats</NavLink>
-            </h2>
-            <h2>
-              <NavLink to='groupchats'>
-                <FontAwesomeIcon icon={faUserGroup} />
-              </NavLink>
-            </h2>
-            <h2>
-              <NavLink to='contacts'>
-                <FontAwesomeIcon icon={faAddressBook} />
-              </NavLink>
-            </h2>
-            <h2>
-              <NavLink to='settings'>Settings</NavLink>
-            </h2>
-          </div>
-          <div className='nav-bottom'>
-            <h2>
-              <NavLink to='darkmode'>DarkMode</NavLink>
-            </h2>
-            <h2>
-              <NavLink to='/'>Profile (photo)</NavLink>
-            </h2>
-          </div>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<Profile />} />
-          <Route path='chats' element={<Chats />} />
-          <Route path='groupchats' element={<GroupChats />} />
-          <Route path='contacts' element={<Contacts />} />
-          <Route path='settings' element={<Settings />} />
-          <Route path='darkmode' element={<DarkMode />} />
-          <Route path='profile' element={<Profile />} />
-        </Routes>
+    <div className='chat-container'>
+      <BrowserRouter>
+        <header>
+          <nav className='nav-container'>
+            <h1 className='nav-top'>ChatVia</h1>
+            <div className='navLink-container'>
+              <h2>
+                <NavLink to='/'>
+                  <FontAwesomeIcon icon={faUser} />
+                </NavLink>
+              </h2>
+              <h2>
+                <NavLink to='chats'>
+                  <FontAwesomeIcon icon={faCommentDots} />
+                </NavLink>
+              </h2>
+              <h2>
+                <NavLink to='groupchats'>
+                  <FontAwesomeIcon icon={faUserGroup} />
+                </NavLink>
+              </h2>
+              <h2>
+                <NavLink to='contacts'>
+                  <FontAwesomeIcon icon={faAddressBook} />
+                </NavLink>
+              </h2>
+              <h2>
+                <NavLink to='settings'>
+                  <FontAwesomeIcon icon={faGear} />
+                </NavLink>
+              </h2>
+            </div>
+            <div className='nav-bottom'>
+              <h2>
+                <NavLink to='darkmode'>
+                  <FontAwesomeIcon icon={faCircleHalfStroke} />
+                </NavLink>
+              </h2>
+              <h2>
+                <NavLink to='/'>Profile (photo)</NavLink>
+              </h2>
+            </div>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route index element={<Profile />} />
+            <Route path='chats' element={<Chats />} />
+            <Route path='groupchats' element={<GroupChats />} />
+            <Route path='contacts' element={<Contacts />} />
+            <Route path='settings' element={<Settings />} />
+            <Route path='darkmode' element={<DarkMode />} />
+            <Route path='profile' element={<Profile />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+      <div>
         <p>Chat component</p>
-      </main>
-    </BrowserRouter>
+      </div>
+    </div>
   )
 }
 
