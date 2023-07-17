b3e1ebc0ab1e2dfd729ed202d656cd9cf64975ac
MattMcKay <matthewmckay@gmail.com>
2023-07-17 16:20:35 -0400

    Fixed some issues with layout of chats section of sidebar. Moving on to work on main chat area

--- vite-chatvia-project/src/layouts/MainChatLayout.jsx
+++ vite-chatvia-project/src/layouts/MainChatLayout.jsx
@@ -1,17 +1,7 @@
 import React from 'react'
 
 const MainChatLayout = () => {
-  return (
-    <div>
-      <h2>Main Chat area</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus sed unde
-        temporibus, neque earum quidem. Excepturi ratione dolorem nemo, voluptas
-        error consequuntur eaque optio, sit quam explicabo praesentium fugiat.
-        Asperiores?
-      </p>
-    </div>
-  )
+  return <div className='chat'>poop</div>
 }
 
 export default MainChatLayout
