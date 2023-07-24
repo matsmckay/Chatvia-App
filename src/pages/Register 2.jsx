613f73dc132fca1eeb13f42bba2261e90ebcd2c5
MattMcKay <matthewmckay@gmail.com>
2023-07-24 15:39:52 -0400

    Cleaning out duplicate files

--- src/pages/Register 2.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Register = () => {
-  return <h1>Registration page</h1>
-}
-
-export default Register
--- src/pages/chatList/Navbar 2.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Navbar = () => {
-  return <div>Nav bar for chatlist</div>
-}
-
-export default Navbar
