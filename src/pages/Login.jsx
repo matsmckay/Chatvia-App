960c90327619e2511234d690db4317ff167285ab
MattMcKay <matthewmckay@gmail.com>
2023-07-24 16:10:55 -0400

    Preliminary HTML written for login and sign up pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <>
+      <header>
+        <div className='chat-via-logo'>ChatVia</div>
+        <h1>Sign in</h1>
+        <h2>Sign in to continue to ChatVia</h2>
+      </header>
+      <main>
+        <div className='user'>Username</div>
+        <div className='password'>Password</div>
+        <button>Sign in</button>
+      </main>
+      <footer>
+        <h3>Don't have an account? Sign up now!</h3>
+        <h4>Copyright blah blah blah</h4>
+      </footer>
+    </>
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 
 const Register = () => {
-  return <h1>Registration page</h1>
+  return (
+    <>
+      <header>
+        <div className='chat-via-logo'>ChatVia</div>
+        <h1>Sign up</h1>
+        <h2>Get your Chatvia account now</h2>
+      </header>
+      <main>
+        <div className='email'>Email</div>
+        <div className='user'>Username</div>
+        <div className='password'>Password</div>
+        <button>Sign up</button>
+        <div className='terms'>
+          By registering you agree to the Chatvia Terms of Use
+        </div>
+      </main>
+      <footer>
+        <h3>Already have an account? Sign in now!</h3>
+        <h4>Copyright blah blah blah</h4>
+      </footer>
+    </>
+  )
 }
 
 export default Register
