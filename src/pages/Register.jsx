701722eca6326721abb15c2066ecea35c9697c2e
MattMcKay <matthewmckay@gmail.com>
2023-07-25 22:33:33 -0400

    Started building registration page. Basic styling for it as well

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,26 +2,66 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <>
-      <header>
-        <div className='chat-via-logo'>ChatVia</div>
+    <div className='registration-container'>
+      <div className='logo'>Chatvia logo</div>
+      <div className='register-header'>
         <h1>Sign up</h1>
-        <h2>Get your Chatvia account now</h2>
-      </header>
-      <main>
-        <div className='email'>Email</div>
-        <div className='user'>Username</div>
-        <div className='password'>Password</div>
-        <button>Sign up</button>
-        <div className='terms'>
-          By registering you agree to the Chatvia Terms of Use
+        <h3>Get your Chatvia account now</h3>
+      </div>
+      <form id='registration-form' aria-label='Registration Form'>
+        <div className='form-group'>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='text'
+            id='email'
+            name='email'
+            aria-required='true'
+            aria-label='Enter your desired email address'
+            required
+          />
         </div>
-      </main>
-      <footer>
-        <h3>Already have an account? Sign in now!</h3>
-        <h4>Copyright blah blah blah</h4>
-      </footer>
-    </>
+        <div className='form-group'>
+          <label htmlFor='username'>Username</label>
+          <input
+            type='text'
+            id='username'
+            name='username'
+            aria-required='true'
+            aria-label='Enter your desired username'
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='text'
+            id='password'
+            name='password'
+            aria-required='true'
+            aria-label='Enter your desired password'
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='confirm-password'>Password</label>
+          <input
+            type='text'
+            id='confirm-password'
+            name='confirm-password'
+            aria-required='true'
+            aria-label='Confirm your password'
+            required
+          />
+        </div>
+        <button className='submit'>Sign up</button>
+        <div className='terms'>
+          <p>By registering you agree to the Chatvia</p>
+          <p>Terms of Use</p>
+        </div>
+      </form>
+      <div>Already have an account? Signin</div>
+      <p>Copyright blah blah</p>
+    </div>
   )
 }
 
