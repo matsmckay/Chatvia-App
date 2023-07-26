d92670f68c489d72c38a9ff7a0c6acfc1159d0a2
MattMcKay <matthewmckay@gmail.com>
2023-07-26 09:24:46 -0400

    Registered first user using firebase createUserWithEmailAndPassword

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,19 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
   // firebase doesn't automatically create databaseURL so we will do it below
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  databaseURL: import.meta.env.VITE_REACT_APP_DATABASE_URL,
+  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_APP_ID,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+export { auth }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,23 +1,55 @@
-import React from 'react'
+import { useState } from 'react'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../firebase'
 
 const Register = () => {
+  const [data, setData] = useState({
+    email: '',
+    name: '',
+    password: '',
+    error: null,
+    loading: false,
+  })
+
+  const { name, email, password, error, loading } = data
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setData({ ...data, error: null, loading: true })
+    if (!name || !email || !password) {
+      setData({ ...data, error: 'All fields are required' })
+    }
+    try {
+      const result = await createUserWithEmailAndPassword(auth, email, password)
+      console.log(result.user)
+    } catch (error) {}
+  }
+
   return (
     <div className='registration-container'>
       <div className='logo'>Chatvia logo</div>
-      <div className='register-header'>
+      <div className='register-header '>
         <h1>Sign up</h1>
         <h3>Get your Chatvia account now</h3>
       </div>
-      <form id='registration-form' aria-label='Registration Form'>
+      <form
+        id='registration-form'
+        aria-label='Registration Form'
+        onSubmit={handleSubmit}
+      >
         <div className='form-group'>
           <label htmlFor='email'>Email</label>
           <input
             type='text'
             id='email'
             name='email'
+            value={email}
+            onChange={handleChange}
             aria-required='true'
             aria-label='Enter your desired email address'
-            required
+            // required
           />
         </div>
         <div className='form-group'>
@@ -25,35 +57,43 @@ const Register = () => {
           <input
             type='text'
             id='username'
-            name='username'
+            name='name'
+            value={name}
+            onChange={handleChange}
             aria-required='true'
             aria-label='Enter your desired username'
-            required
+            // required
           />
         </div>
         <div className='form-group'>
           <label htmlFor='password'>Password</label>
           <input
-            type='text'
+            type='password'
             id='password'
             name='password'
+            value={password}
+            onChange={handleChange}
             aria-required='true'
             aria-label='Enter your desired password'
-            required
+            // required
           />
         </div>
-        <div className='form-group'>
-          <label htmlFor='confirm-password'>Password</label>
+        {/* <div className='form-group'>
+          <label htmlFor='confirm-password'>Confirm Password</label>
           <input
             type='text'
             id='confirm-password'
             name='confirm-password'
+            value={password}
             aria-required='true'
             aria-label='Confirm your password'
             required
           />
+        </div> */}
+        {error ? <p className='error'>{error}</p> : null}
+        <div className='btn-container'>
+          <button className='btn'>Sign up</button>
         </div>
-        <button className='submit'>Sign up</button>
         <div className='terms'>
           <p>By registering you agree to the Chatvia</p>
           <p>Terms of Use</p>
