4546955f98ffba25b1df8f1fbb35d502236090c0
MattMcKay <matthewmckay@gmail.com>
2023-08-01 11:31:44 -0400

    Working on Profile page and UserAvailabilitiy component. Put in functionality that allows you to click on user image to open your files to select a photo. Photo upload functionality to be done next. Some CSS styling done as well on both page and component

--- src/components/UserAvailability.jsx
+++ src/components/UserAvailability.jsx
@@ -1,12 +1,38 @@
 import currentUserPic from '../assets/users/avatar-1.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCameraRetro } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 
 const UserAvailability = () => {
+  const [img, setImg] = useState('')
   return (
-    <div className='user-available'>
-      <img src={currentUserPic} alt='Profile pic of current user' />
-      <p>user name</p>
-      <p>user availability</p>
-    </div>
+    <section>
+      <div className='availability-container'>
+        <div className='profile-img-container'>
+          <img
+            src={currentUserPic}
+            alt='Profile pic or avatar of current user'
+          />
+          <div className='overlay'>
+            <div>
+              <label htmlFor='photo'>
+                <FontAwesomeIcon icon={faCameraRetro} />
+              </label>
+              <input
+                type='file'
+                accept='image/*'
+                id='photo'
+                onChange={(e) => setImg(e.target.files[0])}
+              />
+            </div>
+          </div>
+        </div>
+        <div className='profile-text-container'>
+          <p>user name</p>
+          <p>user availability</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,29 @@
 import UserAvailability from '../components/UserAvailability'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
 
 const Profile = () => {
   return (
-    <div className='profile-component'>
-      <div className='user'>
-        <h2>My Profile</h2>
+    <div className='profile-container'>
+      <div className='profile-header'>
+        <h1>My Profile</h1>
+        <FontAwesomeIcon icon={faEllipsisVertical} />
       </div>
       <UserAvailability />
+      <div className='quote-container'>
+        "Chatting is like a joyful rollercoaster ride—laugh out loud, type away
+        your dreams, and remember, the emoticons are the spice of life! 🎢😂🚀"
+      </div>
+      <div className='profile-info-container'>
+        <h3>About</h3>
+        <div className='profile-info'>
+          <h4>Name</h4>
+          <h4>Email</h4>
+          <h4>Joined on: ...</h4>
+          <h4>Time</h4>
+          <h4>Location</h4>
+        </div>
+      </div>
     </div>
   )
 }
