7abd06c28a99ea74614dca49043afe54efcdb150
MattMcKay <matthewmckay@gmail.com>
2023-09-16 14:38:14 -0400

    CSS added to main chat layout message area to display messages from logged in user and their chat friends

--- src/pages/mainChat/Message.jsx
+++ src/pages/mainChat/Message.jsx
@@ -6,19 +6,21 @@ const Message = ({ msg, user1 }) => {
   useEffect(() => {
     scrollRef.current?.scrollIntoView({ behavior: 'smooth' })
   }, [msg])
+  console.log(user1)
+  console.log(msg.from)
   return (
     <div
-      className={`message-wrapper ${msg.from === user1 ? 'own' : ''}`}
+      className={`message-wrapper ${msg.from === user1.uid ? 'own' : ''}`}
       ref={scrollRef}
     >
-      <div className='message my-message'>
-        <p>
-          {msg.media ? <img src={msg.media} alt={msg.text} /> : null}
-          {msg.text}
-          <br />
-          <span>{formatDistanceToNow(msg.createdAt)}</span>
-        </p>
-      </div>
+      {/* <div className='message my-message'> */}
+      <p className={msg.from === user1.uid ? 'me' : 'friend'}>
+        {msg.media ? <img src={msg.media} alt={msg.text} /> : null}
+        {msg.text}
+        <br />
+        <span>{formatDistanceToNow(msg.createdAt)}</span>
+      </p>
+      {/* </div> */}
     </div>
   )
 }
